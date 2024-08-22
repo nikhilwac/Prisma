6f8d5dde368793da3f6ab2aa86af62af0c5af626
nikhil.b_wac_node <nikhil.b@webandcrafts.com>
2024-08-22 16:14:19 +0530

    password checking issue resolved

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { AuthPaylodDto } from './dtos/auth.paylod.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,7 +13,8 @@ export class AuthService {
   async validateUser({ email, password }: AuthPaylodDto) {
     const findUser = await this.prisma.admin.findUnique({ where: { email } });
     if (!findUser) return null;
-    if (findUser.password === password) {
+    const isPasswordMatch = await bcrypt.compare(password, findUser.password);
+    if (isPasswordMatch) {
       const { password, ...user } = findUser;
       return this.jwtService.sign(user);
     }
